2fbd90e4e9e42319f49fdbc97c43668a2392dffb
Ross McIntyre <rossmci90@hotmail.co.uk>
2021-01-19 09:04:00 +0000

    feat: added all GET routes, tested with postman

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const fs = require("fs");
+const path = require("path");
 
 const app = express();
 
@@ -8,14 +10,24 @@ app.use(express.urlencoded( { extended: true }));
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('You are on the main page of the app')
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
+
+
+
+app.get("/api/notes", (req, res) => {
+    const rawData = fs.readFileSync(path.join(__dirname, "./db/db.json"));
+    const parsedData = JSON.parse(rawData)
+    res.json(parsedData)
 })
 
 
 
-
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+});
 
 app.listen(PORT, () => {
     console.log('Server listening on port', PORT)
-})
+});
