82fcd5ce3a100070431009a5686bbc1721555a9a
Ross McIntyre <rossmci90@hotmail.co.uk>
2021-01-19 11:09:49 +0000

    fix: set up static file routing, prev not serving js and css

--- app.js
+++ app.js
@@ -20,18 +20,24 @@ app.get("/api/notes", (req, res) => {
     res.json(parsedData);
 });
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, "./public/index.html"));
-});
-
 app.post("/api/notes", (req, res) => {
     const newNote = req.body;
     const rawData = fs.readFileSync(path.join(__dirname, "./db/db.json"));
     const parsedData = JSON.parse(rawData);
     parsedData.push(newNote);
     fs.writeFileSync(path.join(__dirname, "./db/db.json"), JSON.stringify(parsedData))
-    res.status(200).send('Post received and about to parse')
-})
+    res.status(200).send(parsedData)
+});
+
+//this enables us to serve the static js and css files
+
+app.use(express.static(path.join(__dirname, "./public")));
+
+//default route, everything undefined goes to our home page
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+});
 
 app.listen(PORT, () => {
     console.log('Server listening on port', PORT)
